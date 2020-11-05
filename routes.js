0bbd937af97aa05f5615c9e5c4d3849bc38f8945
barbara zujewska <barbara@host-78-65-176-96.homerun.telia.com>
2020-11-05 12:21:25 +0100

    api working

---
+++ routes.js
@@ -0,0 +1,25 @@
+const express = require('express');
+const recipesJson = require('./db/recipes.json');
+
+const router = express.Router();
+
+router.get('/recipes', async (req, res, next) => {
+  try {
+    res.status(200).json(recipesJson);
+  } catch (err) {
+    next(err);
+  }
+});
+
+router.get('/recipes/:id', async (req, res, next) => {
+  try {
+    let recipesData = recipesJson;
+    const resData = recipesJson.data.filter(el => el.id === req.params.id);
+    recipesData.data = resData;
+    res.status(200).json(recipesData);
+  } catch (err) {
+    next(err);
+  }
+});
+
+module.exports = router;
